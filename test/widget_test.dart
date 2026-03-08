c8c1b9028832ab3bb2adb1ce58491c5adab03a6b
im-jonny-sins <nightcallno@gmail.com>
2026-03-09 01:10:57 +0500

    test widget error fixed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,7 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+import 'package:flutter_test/flutter_test.dart';
 
-
-
-void main() {}
+void main() {
+  test('dummy test — чтобы CI не падал пока нет реальных тестов', () {
+    expect(true, isTrue);   // или просто expect(1 + 1, 2);
+  });
+}
